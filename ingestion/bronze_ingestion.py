6b2fb0317e3a1cfc3bd5c1d577e9b440b79388dd
srinadhtDE <srinadht09@gmail.com>
2026-03-10 10:44:12 -0400

    Update bronze_ingestion.py

--- ingestion/bronze_ingestion.py
+++ ingestion/bronze_ingestion.py
@@ -1,1 +1,11 @@
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.appName("Bronze Ingestion").getOrCreate()
+
+df = spark.read.option("header", True).csv("/FileStore/data/transactions.csv")
+
+df.write.format("delta") \
+.mode("overwrite") \
+.saveAsTable("bronze_transactions")
+
+print("Bronze table created")
