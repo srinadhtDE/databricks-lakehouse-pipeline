73e680b743e9efa8febaf4bc876d87c453b1a135
srinadhtDE <srinadht09@gmail.com>
2026-03-10 11:20:04 -0400

    Update silver_to_gold.py

--- transformations/silver_to_gold.py
+++ transformations/silver_to_gold.py
@@ -1,17 +1,26 @@
-from pyspark.sql.functions import sum, countDistinct
+from utils.config_loader import load_config, get_logger
 
-gold_df = spark.sql("""
-SELECT
-    transaction_date,
-    SUM(amount) AS daily_revenue,
-    COUNT(DISTINCT user_id) AS active_users
-FROM silver_transactions
-GROUP BY transaction_date
-""")
+logger = get_logger("gold_aggregation")
+
+config = load_config("../configs/pipeline_config.yaml")
+
+logger.info("Starting Gold layer aggregation")
+
+silver_df = spark.table(config["silver_table"])
+
+gold_df = silver_df.groupByExpr(
+    "date(transaction_timestamp) as transaction_date"
+).aggExpr(
+    "count(*) as total_transactions",
+    "sum(transaction_amount) as total_revenue",
+    "count(distinct user_id) as active_users",
+    "avg(transaction_amount) as avg_transaction_value",
+    "sum(fraud_flag) as fraud_transactions"
+)
 
 gold_df.write \
     .format("delta") \
     .mode("overwrite") \
-    .saveAsTable("gold_daily_metrics")
+    .saveAsTable(config["gold_table"])
 
-print("Gold analytics table created")
+logger.info("Gold metrics table successfully created")
