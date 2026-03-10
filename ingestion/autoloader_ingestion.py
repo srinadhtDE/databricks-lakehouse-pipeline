287bc1371fb7828244a223a8cf170d0fad3eda3c
srinadhtDE <srinadht09@gmail.com>
2026-03-10 13:10:25 -0400

    Update autoloader_ingestion.py

--- ingestion/autoloader_ingestion.py
+++ ingestion/autoloader_ingestion.py
@@ -1,27 +1,62 @@
 from pyspark.sql.types import *
-from configs.databricks_config import *
+from utils.config_loader import load_config, get_logger
+
+# --------------------------------------------
+# Initialize Logger and Config
+# --------------------------------------------
+
+logger = get_logger("bronze_ingestion")
+
+config = load_config("../configs/pipeline_config.yaml")
+
+logger.info("Starting Bronze layer ingestion using Databricks Auto Loader")
+
+# --------------------------------------------
+# Transaction Schema Definition
+# --------------------------------------------
 
 transaction_schema = StructType([
-    StructField("transaction_id", IntegerType()),
-    StructField("user_id", IntegerType()),
-    StructField("merchant_id", IntegerType()),
-    StructField("transaction_amount", DoubleType()),
-    StructField("transaction_type", StringType()),
-    StructField("payment_method", StringType()),
-    StructField("transaction_status", StringType()),
-    StructField("transaction_timestamp", TimestampType()),
-    StructField("country", StringType()),
-    StructField("fraud_flag", IntegerType())
+    StructField("transaction_id", IntegerType(), False),
+    StructField("user_id", IntegerType(), True),
+    StructField("merchant_id", IntegerType(), True),
+    StructField("transaction_amount", DoubleType(), True),
+    StructField("transaction_type", StringType(), True),
+    StructField("payment_method", StringType(), True),
+    StructField("transaction_status", StringType(), True),
+    StructField("transaction_timestamp", TimestampType(), True),
+    StructField("country", StringType(), True),
+    StructField("fraud_flag", IntegerType(), True)
 ])
 
-df = spark.readStream \
+logger.info("Schema defined for ingestion")
+
+# --------------------------------------------
+# Auto Loader Streaming Ingestion
+# --------------------------------------------
+
+stream_df = spark.readStream \
     .format("cloudFiles") \
     .option("cloudFiles.format", "csv") \
     .option("header", True) \
     .schema(transaction_schema) \
-    .load(DATA_PATH)
+    .load(config["data_path"])
 
-df.writeStream \
+logger.info("Auto Loader streaming source configured")
+
+# --------------------------------------------
+# Write to Bronze Delta Table
+# --------------------------------------------
+
+query = stream_df.writeStream \
     .format("delta") \
-    .option("checkpointLocation", CHECKPOINT_PATH) \
-    .table(BRONZE_TABLE)
+    .option("checkpointLocation", config["checkpoint_path"]) \
+    .outputMode("append") \
+    .table(config["bronze_table"])
+
+logger.info(f"Streaming ingestion started for Bronze table: {config['bronze_table']}")
+
+# --------------------------------------------
+# Monitor Stream
+# --------------------------------------------
+
+query.awaitTermination()
