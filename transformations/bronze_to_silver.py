5ad54f4d9b9d352241ac89c988676b790b7f7eee
srinadhtDE <srinadht09@gmail.com>
2026-03-10 11:19:36 -0400

    Update bronze_to_silver.py

--- transformations/bronze_to_silver.py
+++ transformations/bronze_to_silver.py
@@ -1,15 +1,25 @@
 from pyspark.sql.functions import col
+from utils.config_loader import load_config, get_logger
 
-bronze_df = spark.table("bronze_transactions")
+logger = get_logger("silver_transformation")
 
-silver_df = bronze_df \
+config = load_config("../configs/pipeline_config.yaml")
+
+logger.info("Starting Silver transformation")
+
+bronze_df = spark.table(config["bronze_table"])
+
+clean_df = bronze_df \
     .dropDuplicates(["transaction_id"]) \
-    .filter(col("amount").isNotNull()) \
-    .withColumn("amount", col("amount").cast("double"))
+    .filter(col("transaction_amount") > 0) \
+    .filter(col("transaction_status") == "completed")
 
-silver_df.write \
+logger.info(f"Cleaned dataset count: {clean_df.count()}")
+
+clean_df.write \
     .format("delta") \
     .mode("overwrite") \
-    .saveAsTable("silver_transactions")
+    .partitionBy(config["partition_column"]) \
+    .saveAsTable(config["silver_table"])
 
-print("Silver table created")
+logger.info("Silver table created successfully")
