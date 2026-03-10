d193cc82704dafd49a5a389130f9d9f649c452f3
srinadhtDE <srinadht09@gmail.com>
2026-03-10 10:48:44 -0400

    Update silver_to_gold.py

--- transformations/silver_to_gold.py
+++ transformations/silver_to_gold.py
@@ -1,1 +1,17 @@
+from pyspark.sql.functions import sum, countDistinct
 
+gold_df = spark.sql("""
+SELECT
+    transaction_date,
+    SUM(amount) AS daily_revenue,
+    COUNT(DISTINCT user_id) AS active_users
+FROM silver_transactions
+GROUP BY transaction_date
+""")
+
+gold_df.write \
+    .format("delta") \
+    .mode("overwrite") \
+    .saveAsTable("gold_daily_metrics")
+
+print("Gold analytics table created")
