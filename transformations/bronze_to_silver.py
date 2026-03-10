c735ef1ca93f36f1ac28911ef5e2a73b1e8af37a
srinadhtDE <srinadht09@gmail.com>
2026-03-10 10:48:20 -0400

    Update bronze_to_silver.py

--- transformations/bronze_to_silver.py
+++ transformations/bronze_to_silver.py
@@ -1,1 +1,15 @@
+from pyspark.sql.functions import col
 
+bronze_df = spark.table("bronze_transactions")
+
+silver_df = bronze_df \
+    .dropDuplicates(["transaction_id"]) \
+    .filter(col("amount").isNotNull()) \
+    .withColumn("amount", col("amount").cast("double"))
+
+silver_df.write \
+    .format("delta") \
+    .mode("overwrite") \
+    .saveAsTable("silver_transactions")
+
+print("Silver table created")
